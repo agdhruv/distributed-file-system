0a78f24d732f7ec5df3732efb3cb9a5f1b48c94d
Harshit <harsit.agarwal_ug19@ashoka.edu.in>
2018-05-02 02:12:47 +0530

    perhaps final commit for nodes

--- node/sendFile.py
+++ node/sendFile.py
@@ -15,27 +15,24 @@ print('Server listening....')
 while True:
     c, addr = s.accept()  # Establish connection with client.
     print('Got connection from', addr)
-    c.send(b'Hello Client!')
-
-    header = b''
+    c.send("Hello Client")
+    fileToServe = b''
 
     while True:
         data = c.recv(1)
-        
+
         if data == b'\n':
             break
-        header += data
+        fileToServe += data
+    f = open(fileToServe, 'rb')
+    l = f.read(1024)
 
-    with open(header, 'wb') as f:
-        print(header, 'opened')
+    while l:
+        c.send(l)
+        print("Sent:", repr(l))
+        l = f.read(1024)
+    f.close()
 
-        while True:
-            # print 'receiving data...'
-            data = c.recv(1024)
-            if not data:
-                break
-            # print 'data = %s' % data
-            f.write(data)
-    print('Successfully received the file')
+    print('Successfully sent the file')
     c.close()
     print('connection closed')
